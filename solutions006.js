8b31141e70233ef68b02d8e46b20401cc47b8637
edmo339 <teggy1@hotmail.com>
2022-05-17 20:45:44 +0100

    solution

--- solutions006.js
+++ solutions006.js
@@ -10,3 +10,11 @@ Notes
 The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 */
+function makeNegative(num) {
+    if (num < 0){
+      return num
+    }
+    else {
+      return num - (num * 2)
+    }
+  }
