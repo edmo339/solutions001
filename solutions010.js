1d2e18a0ba740581fa6941ca4f5b710ced960cf9
edmo339 <teggy1@hotmail.com>
2022-05-24 14:36:47 +0100

    Minimize sum of arrays (7ku)

---
+++ solutions010.js
@@ -0,0 +1,21 @@
+function minSum(arr) {
+  /*
+    Introduction and Warm-up (Highly recommended)
+    Playing With Lists/Arrays Series
+    Task
+    Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+    
+    Notes
+    Array/list will contain positives only .
+    Array/list will always has even size
+    Input >> Output Examples
+    minSum({5,4,2,3}) ==> return (22) 
+    Explanation:
+    The minimum sum obtained from summing each two integers product , 5*2 + 3*4 = 22
+  */
+
+
+
+  console.log(minSum([5,4,2,3]))
+
+  console.log(minSum([12,6,10,26,3,24]))
