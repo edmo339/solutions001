efc976d9157c23f42bd5f2bf6ac555375dede663
edmo339 <teggy1@hotmail.com>
2022-05-24 14:37:41 +0100

    first try

--- solutions010.js
+++ solutions010.js
@@ -13,7 +13,18 @@ function minSum(arr) {
     Explanation:
     The minimum sum obtained from summing each two integers product , 5*2 + 3*4 = 22
   */
-
+    arr.sort((a, b) => a - b)
+    if ( arr.length < 5){
+       return (arr[0] * arr[arr.length -1]) + (arr[1] * arr[arr.length - 2])
+      }
+    else if (arr.length < 7) {
+      return (arr[0] * arr[arr.length -1]) + (arr[1] * arr[arr.length - 2]) + (arr[2] * arr[arr.length -3])
+    }
+    else {
+        return (arr[0] * arr[arr.length -1]) + (arr[1] * arr[arr.length - 2]) 
+        + (arr[2] * arr[arr.length -3]) + (arr[3] * arr[arr.length -4])
+    }
+    }
 
 
   console.log(minSum([5,4,2,3]))
