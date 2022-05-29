36fe0ff1042bd21a4d1367aa5e61fcef0a8992c8
edmo339 <teggy1@hotmail.com>
2022-05-29 22:21:31 +0100

    first try

--- solutions015.js
+++ solutions015.js
@@ -7,6 +7,21 @@ Ex: 153, where l = 3 ( the number of digits in 153 )
 Write a function that, given n, returns whether or not n is a Narcissistic Number.
 */
 
+function isNarcissistic(n) {
+    let myFunc = num => Number(num);
+    var intArr = Array.from(String(n), myFunc);
+    
+    let a = intArr.map(num => Math.pow(num, intArr.length))
+    let sum = a.reduce((acc, c) => acc + c, 0)
+    console.log(sum)
+    console.log(intArr)
+    console.log(a)
+    if (sum === n){
+      return true
+    }
+    else return false
+  }
 
+  console.log(isNarcissistic(153))
 
-
+  
