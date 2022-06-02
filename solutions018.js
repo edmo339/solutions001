5531fa00f663161bbde724ad9d68240466a2feeb
edmo339 <teggy1@hotmail.com>
2022-06-02 07:55:08 +0100

    done

--- solutions018.js
+++ solutions018.js
@@ -13,4 +13,6 @@ Examples (input -> output)
 In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 */
 
-
+String.prototype.isUpperCase = function() {
+    return this.toUpperCase() == this;
+  }
