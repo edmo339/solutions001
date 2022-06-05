d69c6ce9eaa71e2c732facbefd8feda8584357e9
edmo339 <teggy1@hotmail.com>
2022-06-05 19:40:51 +0100

    solved (easy)

--- solutions020.js
+++ solutions020.js
@@ -6,4 +6,7 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
 */
 
-
+function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort((a, b) => a - b)
+    return numbers[0] + numbers[1]
+  }
