dd873045ad903a019bc4875e9fca40ad4a0a29c7
Edward Morris <teggy1@hotmail.com>
2022-11-25 10:11:48 +0100

    remove vowels

--- solutions033.js
+++ solutions033.js
@@ -22,7 +22,7 @@ Other languages: return 1 or 0 or refer to Sample Tests.
 
 
 function isPythagoreanTriple(integers) {
-    integers.sort()
+    integers.sort((a, b) => a - b)
     return (Math.pow(integers[0],2) + Math.pow(integers[1],2)) === Math.pow(integers[2],2)? true : false
   }
 
