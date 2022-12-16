e841197bb8c8f7b6dbbc70b644e3d7adb8107a4d
Edward Morris <teggy1@hotmail.com>
2022-12-16 15:28:28 +0100

    adding array entries

---
+++ solutions045.js
@@ -0,0 +1,9 @@
+/*
+Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+describe("Two Sum", () => {
+ it("Should implement two sum", () => {
+  assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
+ });
+});
+*/
