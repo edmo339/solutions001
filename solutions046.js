4d7195e972d4f3735b2028f76aefed844f1eeb20
Edward Morris <teggy1@hotmail.com>
2022-12-19 15:42:53 +0100

    hmmm

--- solutions046.js
+++ solutions046.js
@@ -7,3 +7,7 @@ The returned format must be correct in order to complete this challenge.
 
 Don't forget the space after the closing parentheses!
 */
+
+function createPhoneNumber(numbers){
+    numbers.join('')
+  }
